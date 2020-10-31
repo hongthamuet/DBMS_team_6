b1d5a0fcc0f4cb44f6cdb15422aeca3b975f76bd
hongthamuet <56645746+hongthamuet@users.noreply.github.com>
2020-10-31 21:07:29 +0700

    Create admin_layout

--- ManageBook/routes/web.php
+++ ManageBook/routes/web.php
@@ -16,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function(){
     return view('welcome');
 });
-Route::get('Hello', function () {
-    return "Hello World";
+Route::get('/', function () {
+    return view('admin_layout');
 });
