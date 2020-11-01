7b038241c386cdf8ad6b0f414cedea731ecee352
MaiLinh123 <48786774+MaiLinh123@users.noreply.github.com>
2020-11-01 21:40:54 +0700

    Add Database

---
+++ ManageBook/qlsach.sql
@@ -0,0 +1,9 @@
+-- Cơ sở dữ liệu: `qlbansach`
+
+--
+-- Cấu trúc bảng cho bảng `tbl_admin`
+
+--
+INSERT INTO `tbl_admin` (`admin_id`, `admin_email`, `admin_password`, `admin_name`, `admin_phone`, `created_at`, `update_at`) VALUES
+(1, 'mailinh123', '12345678', 'Mai Linh','0976931294', '06/11/2020', ''),
+(2, 'tham123', '12345678', 'Hồng Thắm','0976931294', '06/11/2020','');
