4e6e37b3162ff37e58c9d212e5748bb3e5b2e833
MaiLinh123 <48786774+MaiLinh123@users.noreply.github.com>
2020-11-07 00:05:49 +0700

    Sửa file

--- ManageBook/routes/web.php
+++ ManageBook/routes/web.php
@@ -89,3 +89,4 @@ Route::post('/save-checkout-customer', 'App\Http\Controllers\CheckoutController@
 //Đơn hàng
 Route::get('/payment', 'App\Http\Controllers\CheckoutController@payment');
 Route::get('/manage-order', 'App\Http\Controllers\CheckoutController@manage_order');
+Route::get('/view-order/{id}', 'App\Http\Controllers\CheckoutController@view_order');
