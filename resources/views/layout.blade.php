6ed59b7ddb47bd2333153bab7e11b435ec06d294
hongthamuet <56645746+hongthamuet@users.noreply.github.com>
2020-11-09 23:59:56 +0700

    update

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,18 +6,18 @@
     <meta name="description" content="">
     <meta name="author" content="">
     <title>PaVaNa - Hiệu sách online cho mọi lứa tuổi</title>
-    <link href="{{asset('/frontend/css/bootstrap.min.css')}}" rel="stylesheet">
-    <link href="{{asset('/frontend/css/font-awesome.min.css')}}" rel="stylesheet">
-    <link href="{{asset('/frontend/css/prettyPhoto.css')}}" rel="stylesheet">
-    <link href="{{asset('/frontend/css/price-range.css')}}" rel="stylesheet">
-    <link href="{{asset('/frontend/css/animate.css')}}" rel="stylesheet">
-    <link href="{{asset('/frontend/css/main.css')}}" rel="stylesheet">
-    <link href="{{asset('/frontend/css/responsive.css')}}" rel="stylesheet">
+    <link href="{{asset('public/frontend/css/bootstrap.min.css')}}" rel="stylesheet">
+    <link href="{{asset('public/frontend/css/font-awesome.min.css')}}" rel="stylesheet">
+    <link href="{{asset('public/frontend/css/prettyPhoto.css')}}" rel="stylesheet">
+    <link href="{{asset('public/frontend/css/price-range.css')}}" rel="stylesheet">
+    <link href="{{asset('public/frontend/css/animate.css')}}" rel="stylesheet">
+    <link href="{{asset('public/frontend/css/main.css')}}" rel="stylesheet">
+    <link href="{{asset('public/frontend/css/responsive.css')}}" rel="stylesheet">
     <!--[if lt IE 9]>
     <script src="js/html5shiv.js"></script>
     <script src="js/respond.min.js"></script>
     <![endif]-->       
-    <link rel="shortcut icon" href="{{('/frontend/images/ico/favicon.ico')}}">
+    <link rel="shortcut icon" href="{{('public/frontend/images/ico/favicon.ico')}}">
     <link rel="apple-touch-icon-precomposed" sizes="144x144" href="images/ico/apple-touch-icon-144-precomposed.png">
     <link rel="apple-touch-icon-precomposed" sizes="114x114" href="images/ico/apple-touch-icon-114-precomposed.png">
     <link rel="apple-touch-icon-precomposed" sizes="72x72" href="images/ico/apple-touch-icon-72-precomposed.png">
@@ -256,11 +256,11 @@
     
 
   
-    <script src="{{asset('/frontend/js/jquery.js')}}"></script>
-    <script src="{{asset('/frontend/js/bootstrap.min.js')}}"></script>
-    <script src="{{asset('/frontend/js/jquery.scrollUp.min.js')}}"></script>
-    <script src="{{asset('/frontend/js/price-range.js')}}"></script>
-    <script src="{{asset('/frontend/js/jquery.prettyPhoto.js')}}"></script>
-    <script src="{{asset('/frontend/js/main.js')}}"></script>
+    <script src="{{asset('public/frontend/js/jquery.js')}}"></script>
+    <script src="{{asset('public/frontend/js/bootstrap.min.js')}}"></script>
+    <script src="{{asset('public/frontend/js/jquery.scrollUp.min.js')}}"></script>
+    <script src="{{asset('public/frontend/js/price-range.js')}}"></script>
+    <script src="{{asset('public/frontend/js/jquery.prettyPhoto.js')}}"></script>
+    <script src="{{asset('public/frontend/js/main.js')}}"></script>
 </body>
 </html>
